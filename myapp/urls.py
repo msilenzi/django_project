46bfc5f4a4bfd27d81ae08531eb010754b5e184f
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-10 15:09:03 -0300

    add views with params

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('about/', views.about),
     path('hello/<int:age>', views.hello),
-    path('info/<str:username>,<int:age>', views.info)
+    path('info/<str:username>,<int:age>', views.info),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasksById),
+    path('tasks/<str:title>', views.tasksByTitle),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task  # Importamos el modelo de la base de datos.
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -15,3 +17,18 @@ def about(request):
 
 def info(request, username: str, age: int):
     return HttpResponse(f"<h1>Hello {username}! You're {age} years old")
+
+
+def projects(request):
+    p = list(Project.objects.values())
+    return JsonResponse(p, safe=False)
+
+
+def tasksById(request, id: int):
+    t = Task.objects.get(id=id)
+    return HttpResponse(f"task title: {t.title}")
+
+
+def tasksByTitle(request, title: str):
+    t = get_object_or_404(Task, title=title)
+    return HttpResponse(f"task id: {t.id}")
