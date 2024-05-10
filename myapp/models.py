ebe069965a4ff8e0314fb3d715510fea50931bac
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-09 23:52:35 -0300

    create Project model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Creamos una clase `Project` que hereda del modelo provisto por Django.
+# Cuando hagamos las migraciones, esta clase se convertira en una tabla.
+# Dentro de esta clase podemos colocar atributos que nos van a permitir
+# especificar las columnas (los datos) que vamos a guardar en una tabla.
+class Project(models.Model):
+    # `name` es el nombre del campo y le asignamos un tipo de dato SQL.
+    name = models.CharField(max_length=255)
