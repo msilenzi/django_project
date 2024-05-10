71a9e0db457584612fdb702c9574eef0d3097b03
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-10 00:11:39 -0300

    add Task model

--- myapp/models.py
+++ myapp/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-# Creamos una clase `Project` que hereda del modelo provisto por Django.
-# Cuando hagamos las migraciones, esta clase se convertira en una tabla.
-# Dentro de esta clase podemos colocar atributos que nos van a permitir
-# especificar las columnas (los datos) que vamos a guardar en una tabla.
+
 class Project(models.Model):
+    """
+    Creamos una clase `Project` que hereda del modelo provisto por Django.
+    Cuando hagamos las migraciones, esta clase se convertira en una tabla.
+    Dentro de esta clase podemos colocar atributos que nos van a permitir
+    especificar las columnas (los datos) que vamos a guardar en una tabla.
+    """
     # `name` es el nombre del campo y le asignamos un tipo de dato SQL.
     name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
