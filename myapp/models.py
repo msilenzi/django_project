282642f49dc1b3beea7f177e66baca879d3e1de7
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-10 19:32:35 -0300

    add Project y Tastk to admin panel

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Project(models.Model):
     """
@@ -13,8 +11,14 @@ class Project(models.Model):
     # `name` es el nombre del campo y le asignamos un tipo de dato SQL.
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return str(self.name)
+
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title + ' - ' + self.project.name
